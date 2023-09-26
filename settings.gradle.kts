aa7edccdfaca2ae5de22110e919d76b31419f1ff
yangyan <yangyan9791@cvte.com>
2023-09-26 11:59:45 +0800

    增加jitpack配置文件

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ pluginManagement {
     repositories {
         google()
         mavenCentral()
+        maven(url = "https://jitpack.io")
         gradlePluginPortal()
     }
 }
@@ -10,9 +11,10 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven(url = "https://jitpack.io")
     }
 }
 
 rootProject.name = "BluetoothDemo"
 include(":app")
-include(":BluetoothManager")
+//include(":BluetoothManager")
